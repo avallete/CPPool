81ef827f23e430c63b6f7c9c02b904632be10161
Andrew VALLETEAU <avallete@e1r3p2.42.fr>
2015-06-11 14:12:19 +0200

    ex01

---
+++ J01/ex01/ex01.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <iostream>
+
+void	memoryLeak()
+{
+	std::string*	panthere = new std::string("String panthere");
+
+	std::cout << *panthere << std::endl;
+	delete panthere;
+	panthere = NULL;
+}
