faa5921537119235cdb23442bd5266341e4281d7
Andrew VALLETEAU <avallete@e1r3p2.42.fr>
2015-06-11 17:36:23 +0200

    ex04 OK

--- J01/ex04/ex04.cpp
+++ J01/ex04/ex04.cpp
@@ -3,12 +3,12 @@
 
 int main(void)
 {
-	std::string str("This is a little cute string !");
+	std::string str("HI THIS IS BRAIN");
 	std::string *ptrstr = &str;
 	std::string& refstr = str;
 	
-	std::cout << "Display " << str << std::endl;
-	std::cout << "Display using ptr " << *ptrstr << std::endl;
-	std::cout << "Display usign ref" << refstr << std::endl;
+	std::cout << str << std::endl;
+	std::cout << *ptrstr << std::endl;
+	std::cout << refstr << std::endl;
 	return (0);
 }
