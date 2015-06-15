c9f831d73046eda7e27a053010474f3229a5da57
Andrew VALLETEAU <avallete@e1r4p1.42.fr>
2015-06-15 13:06:25 +0200

    J00 ex02 + test_script

--- J00/ex02/test_script.sh
+++ J00/ex02/test_script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-g++ -Wall -Wextra -Werror Account.class.cpp Account.class.hpp test.cpp;
+g++ -Wall -Wextra -Werror Account.class.cpp Account.class.hpp tests.cpp;
 cat 19920104_091532.log | cut -c 19- > us;
 ./a.out | cut -c 19- > our;
 diff -c us our;
+rm us our a.out *.gch;
