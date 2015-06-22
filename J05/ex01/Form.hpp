a96d7263ee76d210eb8e9f1a3d29c0200d6742fa
Andrew VALLETEAU <avallete@e1r3p1.42.fr>
2015-06-22 17:14:39 +0200

    ex01

---
+++ J05/ex01/Form.hpp
@@ -0,0 +1,58 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+#include <iostream>
+
+class Bureaucrat;
+
+class Form
+{
+	public:
+		Form(void);
+		Form(std::string name, int sgrade, int xgrade);
+		~Form(void);
+		std::string&	getName(void) const;
+		int				getSGrade(void) const;
+		int				getXGrade(void) const;
+		bool			getSign(void) const;
+		void			beSigned(Bureaucrat& b);
+
+		Form& operator=(Form const & rhs);
+		class GradeTooHighException: public std::exception
+		{
+			private:
+				GradeTooHighException& operator=(GradeTooHighException const & rhs);
+
+			public:
+				GradeTooHighException(GradeTooHighException const & src): std::exception(src)
+				{return;};
+				GradeTooHighException(void){return;};
+				~GradeTooHighException(void) throw(){return;};
+				virtual const char* what() const throw(){
+					return ("Grade Too Hight");
+				};
+		};
+		class GradeTooLowException: public std::exception
+		{
+			private:
+				GradeTooLowException& operator=(GradeTooLowException const & rhs);
+
+			public:
+				GradeTooLowException(GradeTooLowException const & src): std::exception(src)
+				{return;};
+				GradeTooLowException(void)
+				{return;};
+				~GradeTooLowException(void) throw(){return;};
+				virtual const char* what() const throw(){
+					return ("Grade Too Low");
+				};
+		};	
+	private:
+		Form(Form const & src);
+		std::string const	m_name;	
+		int	const			m_sgrade;
+		int	const			m_xgrade;
+		bool				m_sign;
+};
+std::ostream& operator<<(std::ostream&, Form&);
+#include "Bureaucrat.hpp"
+#endif
---
+++ J05/ex01/main_ex01.cpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marene <marene@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/04/12 13:13:18 by marene            #+#    #+#             */
+/*   Updated: 2015/04/12 16:08:18 by marene           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int		main(void)
+{
+	Bureaucrat	bob("bob", 45);
+	Bureaucrat	jim("jim", 150);
+	Form		A4("A4", 45, 45);
+	Form		A5("A5", 150, 21);
+
+	try
+	{
+		Form a("fifi", 0, 2);
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Form a: " << e.what() << std::endl;
+	}
+	try
+	{
+		Form b("fifi", 42, 151);
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Form b: " << e.what() << std::endl;
+	}
+	std::cout << A4 << std::endl << A5 << std::endl << std::endl;
+	std::cout << bob << std::endl << jim << std::endl << std::endl;
+	try
+	{
+		A4.beSigned(jim);
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Form A4: " << e.what() << std::endl;
+	}
+	std::cout << A4 << std::endl << std::endl;
+	try
+	{
+		A4.beSigned(bob);
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Form A4: " << e.what() << std::endl;
+	}
+	std::cout << A4 << std::endl << std::endl;
+	bob.signForm(A4);
+	bob.signForm(A5);
+	jim.signForm(A4);
+}
