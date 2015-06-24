69681e15b92ccfd401eb0bc8a50ad178745703db
Andrew VALLETEAU <avallete@e1r4p1.42.fr>
2015-06-24 03:30:05 +0200

    ex01

--- J07/ex01/whatever.cpp
+++ J07/ex01/whatever.cpp
@@ -10,6 +10,19 @@ void iter(P* ptr, Q len, R f(P))
 	}
 }
 
+template<typename K>
+void showthat(K that)
+{
+	std::cout << that << std::endl;
+}
+
+template<typename F>
+int lenthat(F that)
+{
+	(void)that;
+	return (5);
+}
+
 void print(std::string s)
 {
 	std::cout << "printstring : " << s << std::endl;
@@ -35,12 +48,15 @@ void dec(double i)
 int main(void)
 {
 	int array_int[5] = {42, 43, 44, 54, 56};
-	char array_char[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+	char array_char[11] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 	double array_double[3] = {12, 34e300, 2343234};
-	std::string array_string[3] = {"toto", "titi", "tutu"};
+	std::string array_string[4] = {"toto", "titi", "tutu"};
 
 	iter<int, int, void>(array_int, 5, &inc);
 	iter<char, int, void>(array_char, 10, &ftchar);
 	iter<double, int, void>(array_double, 3, &dec);
 	iter<std::string, int, void>(array_string, 3, &print);
+	iter<char, int, void>(array_char, 4 , showthat<char>);
+	iter<char, int, void>(array_char, 4 , showthat<char>);
+	iter<int, int, void>(array_int, lenthat<int*>(array_int), &inc);
 }
