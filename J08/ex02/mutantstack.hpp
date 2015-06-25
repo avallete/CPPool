86d380310d9a64f80744306e1b606a0499f0c0fe
avallete <avallete@student.42.fr>
2015-06-25 04:40:20 +0200

    begin

--- J08/ex02/mutantstack.hpp
+++ J08/ex02/mutantstack.hpp
@@ -1,16 +1,43 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
+#include <stack>
 #include <list>
 
-class MutantStack
+template<typename T>
+class MutantStack: public std::stack<T>, public virtual std::list<T>
 {
     public:
-        MutantStack(void);
-        MutantStack(MutantStack const & src);
-        ~MutantStack(void);
+        MutantStack(void):std::stack<T>(){};
+        MutantStack(MutantStack const & src):std::stack<T>(src){};
+        ~MutantStack(void){};
+        T& top(void)
+        {
+            return (std::stack<T>::top());
+        }
 
-        MutantStack& operator=(MutantStack const & rhs);
-    private:
-        std::list m_list;
+        void push(const T& value)
+        {
+            std::stack<T>::push(value);
+            std::list<T>::push_front(value);
+            return;
+        }
+
+        void pop()
+        {
+            std::stack<T>::pop();
+            std::list<T>::pop_front();
+            return;
+        }
+        int size()
+        {
+            return (std::stack<T>::size());
+        }
+
+        MutantStack& operator=(MutantStack const & rhs)
+        {
+            std::list<T>::operator=(rhs);
+            std::stack<T>::operator=(rhs);
+            return (*this);
+        };
 };
 #endif
